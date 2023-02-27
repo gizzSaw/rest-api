78ab309d2d46bf64783e67c19a0ca8e01fdfbae8
gizzSaw <gizzsaw@gmail.com>
2023-02-27 19:43:23 +0300

    put

--- app.js
+++ app.js
@@ -1,13 +1,37 @@
 const express = require('express')
 const path = require('path')
+const {v4} = require('uuid')
 const app = express()
 
-const CONTACTS = [
-    {id: 1, name: 'Saveliy', value: '+7-961-100-20-30', marked: false}
+let CONTACTS = [
+    {id: v4(), name: 'Saveliy', value: '+7-961-100-20-30', marked: false}
 ]
 
+app.use(express.json())
+
+//GET
 app.get('/api/contacts', (req, res) => {
     res.status(200).json(CONTACTS)
+})
+
+//POST
+app.post('/api/contacts', (req, res) => {
+    const contact = {...req.body, id: v4(), marked: false}
+    CONTACTS.push(contact)
+    res.status(201).json(contact) 
+})
+
+//DELETE
+app.delete('/api/contacts/:id', (req, res) => {
+    CONTACTS = CONTACTS.filter(c => c.id !== req.params.id)
+    res.status(200).json({message: 'Контакт был удалён'})
+})
+
+//PUT
+app.put('/api/contacts/:id', (req, res) => {
+    const idx = CONTACTS.findIndex(c => c.id === req.params.id)
+    CONTACTS[idx] = req.body
+    res.json(CONTACTS[idx])
 })
 
 app.use(express.static(path.resolve(__dirname, 'client')))
